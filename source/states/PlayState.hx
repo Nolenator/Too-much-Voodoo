ff0f8b5a688cfab1864c50d9c75dbec3d836d57d
Nolenator <nolenator22@gmail.com>
2025-12-04 15:48:05 +0100

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,13 +1,14 @@
 package states;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
-	import flixel.text.FlxText;
+
 	var text = new FlxText(100, 100, 100, "Is this Voodoo, or is it divine intellect?");
 addChild(text);
 	}
