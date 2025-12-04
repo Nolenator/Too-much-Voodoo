073f38af4cebc81b8b5f18c90398b8077e47497d
Nolenator <nolenator22@gmail.com>
2025-12-04 15:41:43 +0100

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -7,12 +7,12 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+		var text = new FlxText(100, 100, 100, "Is this Voodoo, or is it divine intellect?");
+addChild(text);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 	}
-var text = new FlxText(100, 100, 100, "Is this Voodoo, or is it divine intellect?");
-addChild(text);
 }
