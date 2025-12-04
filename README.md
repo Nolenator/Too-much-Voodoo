35d7faabce5e8f8f60e5653e5b1c5ca382a9d7a4
RinasSam <samkhaldoon2006@gmail.com>
2025-12-04 17:15:29 +0300

    Initialization!!!!!

---
+++ README.md
@@ -0,0 +1,1 @@
+Too Much Vodoo:tm:
---
+++ source/Main.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+
+class Main extends Sprite
+{
+
+	public function new()
+	{
+		super();
+		
+		addChild(new FlxGame(0, 0, states.PlayState, 60, 60, true, false));
+	}
+}
